eee56cbf17c9b72f34b8880c8ce7459b5c0590a7
arunjj <jeyam.arun18@gmail.com>
2022-03-25 12:59:22 +0530

    test

---
+++ android/src/main/java/com/reactlibrary/RNNativeToastLibraryModule.java
@@ -0,0 +1,53 @@
+
+package com.reactlibrary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.reactlibrary.view.*;
+
+public class RNNativeToastLibraryModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+  Promise promise;
+
+  public RNNativeToastLibraryModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+    getReactApplicationContext().addActivityEventListener(mActivityEventListener);
+  }
+
+  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+      @Override
+      public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+          Log.e("RESULT CAME HERE","SUCCESS");
+          try{
+            String responsedata = data.getStringExtra("result");
+            promise.resolve(responsedata);
+          }
+          catch(Exception e){}
+      }
+  };
+
+  @ReactMethod
+  public void startPayment(String paymenturl, Promise promise) {
+        this.promise = promise;
+        // Log.e("Passed variable",""+name+" "+location);
+        Intent intent = (new Intent(getReactApplicationContext(), com.reactlibrary.view.WebViewActivity.class)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).putExtra("url",""+paymenturl));
+        getReactApplicationContext().getCurrentActivity().startActivityForResult(intent, 101);
+  }
+
+  @Override
+  public String getName() {
+    return "RNNativeToastLibrary";
+  }
+}
---
+++ android/src/main/java/com/reactlibrary/view/WebViewActivity.java
@@ -0,0 +1,101 @@
+
+package com.reactlibrary.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.reactlibrary.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView wv1 = (WebView) findViewById(R.id.webView);
+//        wv1.setWebViewClient(new MyBrowser());
+
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.loadUrl(getIntent().getStringExtra("url"));
+        wv1.addJavascriptInterface(new WebAppInterface(this), "HTMLOUT");
+        wv1.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl("javascript:window.HTMLOUT.processData(document.getElementById('result').value);");            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Oh no! "+description , Toast.LENGTH_SHORT).show();
+                super.onReceivedError(view, errorCode, description,failingUrl);
+            }
+        });
+    }
+
+    public void tetsme(View view) {
+        Intent intentWithResult = new Intent();
+        intentWithResult.putExtra("result", "Welcome");
+        setResult(Activity.RESULT_OK, intentWithResult);
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface   // must be added for API 17 or higher
+        public void processData(String toast) throws JSONException {
+            // Log.d("Payment Response-->>>", "...>>>>>"+toast);
+            //JSONObject object = new JSONObject(toast);
+            Intent intentWithResult = new Intent();
+            intentWithResult.putExtra("result", toast);
+            setResult(Activity.RESULT_OK, intentWithResult);
+            finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        sendErrorMessage("Payment failed");
+    }
+
+    public void sendErrorMessage(String msg) {
+       Intent intentWithResult= new Intent();
+       intentWithResult.putExtra("ErrorType", "1" );
+       intentWithResult.putExtra("ErrorMsg","Cancelled");
+       setResult(Activity.RESULT_CANCELED,intentWithResult);
+       finish();
+    }
+
+}
